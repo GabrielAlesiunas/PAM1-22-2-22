f0e2e58b23c08fcf12dd3ac85bbd6bf51b191db0
Gabriel Raimundo <gabriel.alesiunas@yahoo.com.br>
2022-02-22 08:58:49 -0300

    criando meu projeto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PamI22/2/22',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
